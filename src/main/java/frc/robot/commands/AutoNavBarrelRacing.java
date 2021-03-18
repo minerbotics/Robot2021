d25139009abc6d988f1c949e12df1c9675f669d7
FRC Team 4827 - Minerbotics <60262671+minerbotics@users.noreply.github.com>
2021-03-17 20:15:50 -0400

    Wednesdays attempt at barrelracing

--- src/main/java/frc/robot/commands/AutoNavBarrelRacing.java
+++ src/main/java/frc/robot/commands/AutoNavBarrelRacing.java
@@ -12,32 +12,33 @@ public class AutoNavBarrelRacing extends SequentialCommandGroup {
 
   
   public AutoNavBarrelRacing(DriveTrain drive) {
-    double turn90 = 0.54;
-    double drive5 = 1.175;
-    double speed = 0.675;
-    double clockwise = 3;
+    double turn90 = 0.78;
+    double drive5 = 1.65;
+    double speed = 0.59;
+    double clockwise = 2.28;
+    double counter = -2.11;
     addCommands(
       new AutoDrive(drive, speed).withTimeout(drive5*2),
       new AutoTurn(drive, clockwise).withTimeout(turn90),
       new AutoDrive(drive, speed).withTimeout(drive5*0.8),
       new AutoTurn(drive, clockwise).withTimeout(turn90),
-      new AutoDrive(drive, speed).withTimeout(drive5),
+      new AutoDrive(drive, speed).withTimeout(drive5*0.8),
       new AutoTurn(drive, clockwise).withTimeout(turn90),
-      new AutoDrive(drive, speed).withTimeout(drive5),
-      new AutoTurn(drive, clockwise).withTimeout(turn90),
-      new AutoDrive(drive, speed).withTimeout(drive5*2.25),
-      new AutoTurn(drive, -clockwise).withTimeout(turn90*0.875),
-      new AutoDrive(drive, speed).withTimeout(drive5*0.875),
-      new AutoTurn(drive, -clockwise).withTimeout(turn90*0.85),
-      new AutoDrive(drive, speed).withTimeout(drive5),
-      new AutoTurn(drive, -clockwise).withTimeout(turn90*0.85),
-      new AutoDrive(drive, speed).withTimeout(drive5*1.75),
-      new AutoTurn(drive, -clockwise).withTimeout(turn90*0.85),
-      new AutoDrive(drive, speed).withTimeout(drive5*1.75),
-      new AutoTurn(drive, -clockwise).withTimeout(turn90*0.85),
-      new AutoDrive(drive, speed).withTimeout(drive5),
-      new AutoTurn(drive, -clockwise).withTimeout(turn90*0.85),
-      new AutoDrive(drive, speed).withTimeout(drive5*3),
+      new AutoDrive(drive, speed).withTimeout(drive5*0.8),
+      new AutoTurn(drive, clockwise).withTimeout(turn90*0.975),
+      new AutoDrive(drive, speed).withTimeout(drive5*2),
+      new AutoTurn(drive, counter).withTimeout(turn90),
+      new AutoDrive(drive, speed).withTimeout(drive5*0.9),
+      new AutoTurn(drive, counter).withTimeout(turn90),
+      new AutoDrive(drive, speed).withTimeout(drive5*0.8),
+      new AutoTurn(drive, counter).withTimeout(turn90),
+      new AutoDrive(drive, speed).withTimeout(drive5*1.6),
+      new AutoTurn(drive, counter).withTimeout(turn90*0.975),
+      new AutoDrive(drive, speed).withTimeout(drive5*1.6),
+      new AutoTurn(drive, counter).withTimeout(turn90*0.985),
+      new AutoDrive(drive, speed).withTimeout(drive5*0.8),
+      new AutoTurn(drive, counter).withTimeout(turn90*0.99),
+      new AutoDrive(drive, speed).withTimeout(drive5*4.435),
       new AutoDrive(drive, -speed).withTimeout(0.15),
       new AutoDrive(drive, 0)
     );
